73b64ebb939fd32a6b414fe4b5369602f0b8e069
sbabureddy <sbabureddy01@gmail.com>
2021-04-25 10:27:17 +0530

    Add Coveralls Badge

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    "collectCoverageFrom": [
-        "src/**/*.{js,jsx}",
-        "!<rootDir>/node_modules/",
-      ],
-      "coverageThreshold": {
-        "global": {
-          "branches": 80,
-          "functions": 80,
-          "lines": 80,
-          "statements": 80
-        }
-      },
-      "coverageReporters": ["text"],
-}
+  collectCoverageFrom: [
+    "!src/index.js",
+    "!src/reportWebVitals.js",
+    "src/**/*.{js,jsx}",
+    "!<rootDir>/node_modules/",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ["text"],
+};
